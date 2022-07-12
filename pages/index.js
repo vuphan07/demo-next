386263ef92bf15a3c078dba34aade3eaf5b67c93
Phan Vu <vu.phan@ntq-solution.com.vn>
2022-07-12 15:25:22 +0700

    test seo

--- pages/index.js
+++ pages/index.js
@@ -43,22 +43,19 @@ _metaUrl.searchParams.set("u", shareUrl);
 //   },
 // ];
 
-
 export default function Home(props) {
   return (
     <div className={styles.container}>
       <Head>
         <meta name="title" content="title demo 1" />
         <meta name="theme-color" content="#F7F0E9" />
-        <meta name="description" content="description page demo 1 app main" />
-        <meta key="1" name="twitter:card" content="summary_large_image 213213123" />
-        <meta key="2" name="twitter:title" content="title page 3123123123" />
-        <meta key="3" name="twitter:image" content="https://live.staticflickr.com/1860/43526893585_cc37ec703a_b.jpg" />
-        <meta key="4" property="og:title" content="title page demo 12132312312" />
-        <meta key="5"
-          property="og:image"
-          content="https://live.staticflickr.com/1860/43526893585_cc37ec703a_b.jpg"
+        <meta
+          name="description"
+          content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, "
         />
+        {props.data.map((prop, index) => (
+          <meta key={index} {...prop} />
+        ))}
         <meta name="format-detection" content="telephone=no" />
         <meta name="msapplication-TileColor" content="#F7F0E9" />
         <meta name="application-name" content="senkyo" />
@@ -106,79 +103,86 @@ export default function Home(props) {
   );
 }
 
-export async function getServerSideProps() {
-  const number = Math.round(Math.random() *100) 
+export async function getServerSideProps(context) {
+  const number = context.query.id || 2;
   let data = [];
-  if(number%2 === 0) {
-     data = [
+  if (number % 2 === 0) {
+    data = [
       {
         name: "description",
-        content:
-          "yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy 123",
+        content: "description cua id chan",
       },
       {
-        name: "og:image",
+        key: "5",
+        property: "og:image",
         content:
           "https://live.staticflickr.com/7581/15907563980_6a18386eed_b.jpg",
       },
       {
-        name: "og:title",
-        content: "title page index",
+        key: "4",
+        property: "og:title",
+        content: "title cau id chan",
       },
       {
         name: "title",
-        content: "title main",
+        content: "title cau id chan",
       },
       {
+        key: "1",
         name: "twitter:card",
         content: "summary_large_image",
       },
       {
+        key: "2",
         name: "twitter:title",
-        content: "title page index",
+        content: "title cau id chan",
       },
       {
+        key: "3",
         name: "twitter:image",
         content:
           "https://live.staticflickr.com/7581/15907563980_6a18386eed_b.jpg",
       },
     ];
-  }else {
-     data = [
+  } else {
+    data = [
       {
         name: "description",
-        content:
-          "yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy 234",
+        content: "description cua id le",
       },
       {
-        name: "og:image",
+        key: "5",
+        property: "og:image",
         content:
           "https://i.9mobi.vn/cf/Images/tt/2021/8/20/anh-avatar-dep-45.jpg",
       },
       {
-        name: "og:title",
+        key: "4",
+        property: "og:title",
         content: "title page index",
       },
       {
         name: "title",
-        content: "title main",
+        content: "title cau id le",
       },
       {
+        key: "1",
         name: "twitter:card",
         content: "summary_large_image",
       },
       {
+        key: "2",
         name: "twitter:title",
-        content: "title page index",
+        content: "title cau id le",
       },
       {
         name: "twitter:image",
+        key: "3",
         content:
           "https://i.9mobi.vn/cf/Images/tt/2021/8/20/anh-avatar-dep-45.jpg",
       },
     ];
   }
-  
 
   return { props: { data } };
 }
